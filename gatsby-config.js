11e07868a307c2876523e00b8fa2a0918b4f38d8
Javan Graham <javanag8@gmail.com>
2020-01-12 21:51:02 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-122866871-1",
+        head: true,
+      },
+    },
+  ]
 }

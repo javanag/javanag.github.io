80c288e8b76acb90ce2cc5610d1fd56470d89c79
Javan Graham <javanag8@gmail.com>
2020-05-19 16:11:47 -0400

    Update intro, revise recent work experience

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import Icon from "./Icon"
 
-const WorkExperience = ({ company, companyUrl, subtitle, children }) => (
+const WorkExperience = ({ company, companyUrl, subtitle, role, children }) => (
   <div className="entry">
     <h3><a href={companyUrl}>{company}</a></h3>
-    <h4 className="subtitle"><Icon icon="fas fa-map-marker-alt"/> {subtitle}</h4>
+    {role &&
+      <h4 className="subtitle"><Icon icon="fa fa-briefcase"/> {role}</h4>
+    }
+    {subtitle &&
+      <h5 className="subtitle"><Icon icon="fas fa-map-marker-alt"/> {subtitle}</h5>
+    }
     {children}
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -54,22 +54,22 @@ export default () => {
       <Section icon="fa fa-user" text="About">
         <p>
           Howdy, I'm Javan, a student in the Computer Science Specialist program at
-          the <Icon icon="fa fa-university" /> University of Toronto. After completing my third year, I have been
-          on <a href="https://web.cs.toronto.edu/undergraduate/pey">PEY</a> working
-          at <a href="https://www.overbond.com/"> <Icon icon="fa fa-briefcase" /> Overbond</a> since May 2019 as a
-          software developer.
+          the <Icon icon="fa fa-university"/> University of Toronto. Starting May 2019, after having completed my
+          third year, I spent a year working as a software developer at a fintech startup as part of 
+          my <a href="https://web.cs.toronto.edu/undergraduate/pey"><Icon icon="fas fa-user-tie" /> PEY</a>.
 
-          After my internship I look forward to returning to school in the fall to complete my studies and graduate
-          in <Icon icon="fa fa-graduation-cap" /> 2021.
+          This summer I am taking some courses, trying my hand at contracting, and having fun with personal projects.
         </p>
 
         <p>
-          Read on to see some selected projects and work experience. I first got into programming as a kid by playing
-          Minecraft and being inspired to learn how to create a game like that. <i>Click around to discover some easter
-          eggs on the page!</i> Check out my <a href="https://github.com/javanag"> <Icon icon="fab fa-github-alt" /> GitHub</a> to
-          see more, peep my <a href="https://ca.linkedin.com/in/javan-graham-baabb1195"> <Icon icon="fab fa-linkedin" /> LinkedIn</a>,
-          and send me an email at <span className="email"><Icon icon="fa fa-envelope" />
-            <b> javan.graham (at) mail.utoronto.ca</b></span> if you'd like to discuss anything!
+          I first got into programming as a kid by playing <a href="https://www.minecraft.net/en-us/">Minecraft</a> and
+          being inspired to learn how to create a game
+          like that. <i>Click around to discover some easter eggs and links on the page!</i> Check out my
+          <a href="https://github.com/javanag"> <Icon icon="fab fa-github-alt" /> GitHub</a> to
+          see more, my <a href="https://ca.linkedin.com/in/javan-graham-baabb1195">
+          <Icon icon="fab fa-linkedin" /> LinkedIn</a> (if you are into that), and send me an email
+          at <span className="email"><Icon icon="fa fa-envelope" /><b> javan.graham (at) mail.utoronto.ca</b></span> if
+          you'd like to discuss anything!
         </p>
       </Section>
 
@@ -88,16 +88,17 @@ export default () => {
         <WorkExperience
           company="Overbond"
           companyUrl="https://www.overbond.com/"
-          subtitle="Toronto, May 2019 - Now"
+          subtitle="Toronto, May 2019 - May 2020"
+          role="Full Stack Developer"
         >
           <p>
-            I recently started an ongoing internship here as a <Icon icon="fa fa-briefcase" /> <b>Full Stack Developer</b>.
-            Responsibilities include:
+            Due to being on such a small team, I took the opportunity to gain broad experience by wearing many hats:
           </p>
           <ul>
-            <li>Implementing features for flagship <Icon icon="fas fa-gem" /> Ruby on Rails and <Icon icon="fab fa-react" /> React web app</li>
-            <li>Creating applications to intake data from financial APIs into <Icon icon="fas fa-database" />  Postgres using <Icon icon="fab fa-python" /> Python</li>
-            <li>Writing design docs, planning sprints, maintaining documentation and tests together with code changes</li>
+            <li>Implementing internal and client requested/demo features for main <Icon icon="fas fa-gem" /> Ruby on Rails web application, with a <Icon icon="fab fa-react" /> React front end, feeding from a <Icon icon="fas fa-database" /> Postgres database, all deployed on <Icon icon="fas fa-server" /> Heroku.</li>
+            <li>Built ETL applications in <Icon icon="fab fa-python" /> Python, containerized with <Icon icon="fab fa-docker" /> Docker and running as Kubernetes pods, to capture real time financial data before forwarding through <Icon icon="fab fa-aws" /> Kinesis for further processing, eventually being stored in <Icon icon="fas fa-database" /> RDS.</li>
+            <li>Participated in hiring other software developer applicants by <Icon icon="fas fa-chalkboard-teacher" /> conducting interviews, <Icon icon="fas fa-tasks" /> grading online assessment submissions, and <Icon icon="fas fa-lightbulb" /> creating new assessments tailored to evaluate the desired skill sets.</li>
+            <li>Other developer tasks, like writing <Icon icon="fas fa-file-alt" /> design docs, maintaining other <Icon icon="fas fa-book" /> documentation wikis, and <Icon icon="fas fa-running" /> planning sprints.</li>
           </ul>
         </WorkExperience>
 
@@ -107,23 +108,20 @@ export default () => {
           subtitle="Burlington, July 2018 - August 2018"
         >
           <p>
-            I got the opportunity to create a static site as part of a marketing campaign for a pilot event the Rotary
+            I created a static site as part of a marketing campaign for a pilot event the Rotary
             Club was hosting the night before <a href="http://www.canadaslargestribfest.com/">Canada's Largest Ribfest</a>.
-            I learned how to:
           </p>
 
-          <ul>
-            <li>
-              Interpret club needs for web page, worked with marketing team to implement design as
-              static site using <Icon icon="fa fa-code" /> HTML, CSS and JavaScript with Bootstrap
-            </li>
-            <li>Track ad campaign impact on web page traffic with Google Analytics</li>
-          </ul>
+          <p>
+            After interpreting the club's needs for web page, I worked with the marketing team to implement the design
+            using <Icon icon="fa fa-code" /> HTML, CSS and JavaScript with Bootstrap. After publishing,
+            we tracked ad campaign impact on web page traffic with Google Analytics.
+          </p>
 
           <p>
-            Once published, it was <Icon icon="fas fa-eye" /> viewed by thousands of unique visitors
-            during the month of August. I'm glad that my first user-facing site (and first attempt at consulting-style
-            work) had a chance to provide real impact.
+            In August, the page was <Icon icon="fas fa-eye" /> viewed by thousands of unique visitors.
+            I'm glad that my first user-facing site, and first attempt at consulting-style
+            work, had a chance to provide real impact.
           </p>
         </WorkExperience>
       </Section>

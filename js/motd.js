8d656da615220eaab1d2b2b210378e7580df4b70
javanag <javanag@users.noreply.github.com>
2023-02-01 03:42:40 +0000

    deploy: 49b9320e8dd051d8b912c89c0399bd1ce75bd236

--- js/motd.js
+++ js/motd.js
@@ -5,22 +5,27 @@ function generateUpdateNextDescription() {
     "amateur guitarist",
     "anime viewer",
     "blockchain baller",
-    "dumpster (treasure) diver",
+    "certified hooper",
+    "dumpster-adjacent diver",
     "ex dollarama cashier",
     "fervent recycler",
+    "former (acting) co-CTO",
+    "grapheneos pixel toter",
     "high school jazz trumpeter",
     "meal prepper",
-    "night walker",
+    "minimalist",
+    "morning and night walker",
     "plant protector",
     "present-day present-time hair dangler",
     "regular gymgoer",
     "retired barber",
     "rusty german speaker",
+    "self (and other) improver",
     "semi-frequent reader",
     "slightly washed jogger",
     "soundcloud producer",
     "startup dreamer",
-    "ttc rider",
+    "ttc passenger",
     "twitch streamer",
     "ultimate frisbee player",
     "veteran gamer",
@@ -28,16 +33,24 @@ function generateUpdateNextDescription() {
     "wadoryu shodan",
   ];
 
+  const motdArticleElement = document.getElementById("motd-article");
+
   return function updateNextDescription(event) {
     if (descriptions.length === 0) {
-      event.target.textContent = "-ll out of things to say";
+      motdArticleElement.remove();
+      event.target.textContent = " all out of things to tell you";
       return;
     }
 
     const sampleIndex = Math.floor(Math.random() * descriptions.length);
     const [ description ] = descriptions.splice(sampleIndex, 1);
 
-    console.log(description);
+    if (["a", "e", "i", "o", "u"].some((vowel) => description.startsWith(vowel))) {
+      motdArticleElement.textContent = " an "
+    } else {
+      motdArticleElement.textContent = " a "
+    }
+
     event.target.textContent = description;
   };
 }
